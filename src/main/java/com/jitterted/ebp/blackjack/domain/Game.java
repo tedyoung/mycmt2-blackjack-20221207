c7d4265d2448d121467c64765d9513beb6c162d6
Ted M. Young <tedyoung@gmail.com>
2022-12-07 15:08:09 -0800

    LAB #3: Completed "purification" of the Game class, moving all I/O related code to Console classes.

--- src/main/java/com/jitterted/ebp/blackjack/domain/Game.java
+++ src/main/java/com/jitterted/ebp/blackjack/domain/Game.java
@@ -1,13 +1,5 @@
 package com.jitterted.ebp.blackjack.domain;
 
-import com.jitterted.ebp.blackjack.adapter.in.console.ConsoleHand;
-import org.fusesource.jansi.Ansi;
-import org.fusesource.jansi.AnsiConsole;
-
-import java.util.Scanner;
-
-import static org.fusesource.jansi.Ansi.ansi;
-
 public class Game {
 
     private final Deck deck;
@@ -15,44 +7,6 @@ public class Game {
     private final Hand dealerHand = new Hand();
     private final Hand playerHand = new Hand();
     private boolean playerDone;
-
-    public static void main(String[] args) {
-        displayWelcomeScreen();
-        waitForEnterFromUser();
-
-        playGame();
-
-        resetScreen();
-    }
-
-    public static void resetScreen() {
-        System.out.println(ansi().reset());
-    }
-
-    private static void playGame() {
-        Game game = new Game();
-        game.initialDeal();
-        game.play();
-    }
-
-    public static void waitForEnterFromUser() {
-        System.out.println(ansi()
-                                   .cursor(3, 1)
-                                   .fgBrightBlack().a("Hit [ENTER] to start..."));
-
-        System.console().readLine();
-    }
-
-    public static void displayWelcomeScreen() {
-        AnsiConsole.systemInstall();
-        System.out.println(ansi()
-                                   .bgBright(Ansi.Color.WHITE)
-                                   .eraseScreen()
-                                   .cursor(1, 1)
-                                   .fgGreen().a("Welcome to")
-                                   .fgRed().a(" JitterTed's")
-                                   .fgBlack().a(" BlackJack game"));
-    }
 
     public Game() {
         deck = new Deck();
@@ -63,33 +17,23 @@ public class Game {
         dealRoundOfCards();
     }
 
-    public void play() {
-        playerTurn();
-
-        dealerTurn();
-
-        displayFinalGameState();
-
-        determineOutcome();
-    }
-
     private void dealRoundOfCards() {
         // why: players first because this is the rule
         playerHand.drawFrom(deck);
         dealerHand.drawFrom(deck);
     }
 
-    public void determineOutcome() {
+    public String determineOutcome() {
         if (playerHand.isBusted()) {
-            System.out.println("You Busted, so you lose.  💸");
+            return "You Busted, so you lose.  💸";
         } else if (dealerHand.isBusted()) {
-            System.out.println("Dealer went BUST, Player wins! Yay for you!! 💵");
+            return "Dealer went BUST, Player wins! Yay for you!! 💵";
         } else if (playerHand.beats(dealerHand)) {
-            System.out.println("You beat the Dealer! 💵");
+            return "You beat the Dealer! 💵";
         } else if (playerHand.pushes(dealerHand)) {
-            System.out.println("Push: Nobody wins, we'll call it even.");
+            return "Push: Nobody wins, we'll call it even.";
         } else {
-            System.out.println("You lost to the Dealer. 💸");
+            return "You lost to the Dealer. 💸";
         }
     }
 
@@ -102,70 +46,22 @@ public class Game {
         }
     }
 
-    private void playerTurn() {
-        // get Player's decision: hit until they stand, then they're done (or they go bust)
-
-        while (!playerHand.isBusted()) {
-            displayGameState();
-            String playerChoice = inputFromPlayer().toLowerCase();
-            if (playerChoice.startsWith("s")) {
-                break;
-            }
-            if (playerChoice.startsWith("h")) {
-                playerHand.drawFrom(deck);
-                if (playerHand.isBusted()) {
-                    return;
-                }
-            } else {
-                System.out.println("You need to [H]it or [S]tand");
-            }
-        }
+    public Hand playerHand() {
+        return playerHand;
     }
 
-    public String inputFromPlayer() {
-        System.out.println("[H]it or [S]tand?");
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
-    }
-
-    public void displayGameState() {
-        System.out.print(ansi().eraseScreen().cursor(1, 1));
-        System.out.println("Dealer has: ");
-        System.out.println(ConsoleHand.displayFaceUpCard(dealerHand));
-
-        // second card is the hole card, which is hidden, or "face down"
-        displayBackOfCard();
-
-        System.out.println();
-        System.out.println("Player has: ");
-        System.out.println(ConsoleHand.cardsAsString(playerHand));
-        System.out.println(" (" + playerHand.value() + ")");
-    }
-
-    private void displayBackOfCard() {
-        System.out.print(
-                ansi()
-                        .cursorUp(7)
-                        .cursorRight(12)
-                        .a("┌─────────┐").cursorDown(1).cursorLeft(11)
-                        .a("│░░░░░░░░░│").cursorDown(1).cursorLeft(11)
-                        .a("│░ J I T ░│").cursorDown(1).cursorLeft(11)
-                        .a("│░ T E R ░│").cursorDown(1).cursorLeft(11)
-                        .a("│░ T E D ░│").cursorDown(1).cursorLeft(11)
-                        .a("│░░░░░░░░░│").cursorDown(1).cursorLeft(11)
-                        .a("└─────────┘"));
-    }
-
-    public void displayFinalGameState() {
-        System.out.print(ansi().eraseScreen().cursor(1, 1));
-        System.out.println("Dealer has: ");
-        System.out.println(ConsoleHand.cardsAsString(dealerHand));
-        System.out.println(" (" + dealerHand.value() + ")");
-
-        System.out.println();
-        System.out.println("Player has: ");
-        System.out.println(ConsoleHand.cardsAsString(playerHand));
-        System.out.println(" (" + playerHand.value() + ")");
+    // QUERY RULES: What options do we have?
+    // -> Snapshot
+    // -> Integrity/Encapsulation
+    // -> Immutable
+    // -> Not mislead caller
+    // Options:
+    // 1. Return copy of Hand: misleads caller
+    // 2. List<Card>/Stream<Card>: doesn't have the value() nor faceUpCard() method/behavior
+    // ==> 3. New Class: HandView ==> Value Object
+    // 4. Interface: ReadOnlyHand -> wrap the underlying Hand, but NOT a Snapshot
+    public Hand dealerHand() {
+        return dealerHand; // make a copy
     }
 
     public boolean isPlayerDone() {
