be901bd4075867711629a05d30dca66486c62404
Ted M. Young <tedyoung@gmail.com>
2022-12-07 16:19:48 -0800

    LAB #4: ConsoleGame uses the display string from the enum

--- src/main/java/com/jitterted/ebp/blackjack/adapter/in/console/ConsoleGame.java
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/in/console/ConsoleGame.java
@@ -29,7 +29,7 @@ public class ConsoleGame {
 
         displayFinalGameState();
 
-        System.out.println(game.determineOutcome());
+        System.out.println(game.determineOutcome().displayString());
 
         resetScreen();
     }
