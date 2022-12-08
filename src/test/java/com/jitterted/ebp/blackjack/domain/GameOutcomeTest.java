a3c97828a22ca9933a0a5c92b4899b4a25de6ef4
Ted M. Young <tedyoung@gmail.com>
2022-12-07 16:04:27 -0800

    LAB #4: Added StubDeck to control which cards are dealt to ensure the player goes bust.

---
+++ src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
@@ -0,0 +1,22 @@
+package com.jitterted.ebp.blackjack.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class GameOutcomeTest {
+
+    @Test
+    void playerHitsAndGoesBustThenOutcomeIsPlayerLoses() {
+        Deck deck = new StubDeck(Rank.TEN, Rank.EIGHT,
+                                 Rank.QUEEN, Rank.JACK,
+                                 Rank.THREE);
+        Game game = new Game(deck);
+        game.initialDeal();
+
+        game.playerHits();
+
+        assertThat(game.determineOutcome())
+                .isEqualTo("You Busted, so you lose.  💸");
+    }
+}
